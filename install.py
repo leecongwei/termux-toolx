a360828a9276f322be1edb8c1954b9eebca7e461
leecongwei <79183004+leecongwei@users.noreply.github.com>
2021-11-25 10:26:49 +0800

    install.py

--- install.py
+++ install.py
@@ -15,23 +15,23 @@ class tool:
       system=sys()
       os.system("clear")
       logo.ins_tnc()
-      inp=input("\033[1;33m 前方施工不能下载 [不给你开/没有选择]> \033[00m")                               
-      if inp=="y" or inp=="yes":
+      inp=input("\033[1;33m 刚刚改好了一些漏洞，帮个忙看看 [Y/yes]> \033[00m")                               
+      if inp=="Y" or inp=="yes":
         os.system("clear")
         logo.installing()
         if system.sudo is not None:
           #require root permission
-          if os.path.exists(system.conf_dir+"/termux-toolx"):
+          if os.path.exists(system.conf_dir+"/Tool-X"):
             pass                                                                                               
           else:
-            os.system(system.sudo+" mkdir "+system.conf_dir+"/termux-toolx")
-          os.system(system.sudo+" cp -r modules core termux-toolx.py "+system.conf_dir+"/termux-toolx")
-          os.system(system.sudo+" cp -r core/termux-toolx "+system.bin)
+            os.system(system.sudo+" mkdir "+system.conf_dir+"/Tool-X")
+          os.system(system.sudo+" cp -r modules core Tool-X.py "+system.conf_dir+"/Tool-X")
+          os.system(system.sudo+" cp -r core/Tool-X "+system.bin)
           os.system(system.sudo+" cp -r core/toolx "+system.bin)
-          os.system(system.sudo+" chmod +x "+system.bin+"/termux-toolx")
+          os.system(system.sudo+" chmod +x "+system.bin+"/Tool-X")
           os.system(system.sudo+" chmod +x "+system.bin+"/toolx")
-          os.system("cd .. && "+system.sudo+" rm -rf termux-toolx")
-          if os.path.exists(system.bin+"/termux-toolx") and os.path.exists(system.conf_dir+"/termux-toolx"):
+          os.system("cd .. && "+system.sudo+" rm -rf Tool-X")
+          if os.path.exists(system.bin+"/Tool-X") and os.path.exists(system.conf_dir+"/Tool-X"):
             os.system("clear")
             logo.ins_sc()
             tmp=input("\033[1;36m ##> \033[00m")
@@ -42,17 +42,17 @@ class tool:
             tmp=input("\033[1;36m ##> \033[00m")
             break
         else:
-          if os.path.exists(system.conf_dir+"/termux-toolx"):
+          if os.path.exists(system.conf_dir+"/Tool-X"):
             pass
           else:
-            os.system("mkdir "+system.conf_dir+"/termux-toolx")
-          os.system("cp -r modules core termux-toolx.py "+system.conf_dir+"/termux-toolx")
-          os.system("cp -r core/termux-toolx"+system.bin)
+            os.system("mkdir "+system.conf_dir+"/Tool-X")
+          os.system("cp -r modules core Tool-X.py "+system.conf_dir+"/Tool-X")
+          os.system("cp -r core/Tool-X"+system.bin)
           os.system("cp -r core/toolx "+system.bin)
-          os.system("chmod +x "+system.bin+"/termux-toolx")
+          os.system("chmod +x "+system.bin+"/Tool-X")
           os.system("chmod +x "+system.bin+"/toolx")
-          os.system("cd .. && rm -rf termux-toolx")
-          if os.path.exists(system.bin+"/termux-toolx") and os.path.exists(system.conf_dir+"/termux-toolx"):
+          os.system("cd .. && rm -rf Tool-X")
+          if os.path.exists(system.bin+"/Tool-X") and os.path.exists(system.conf_dir+"/Tool-X"):
             os.system("clear")
             logo.ins_sc()
             tmp=input("\033[1;36m ##> \033[00m")
