cf2697c89dc74dde4869805d26b215d9875a3f92
leecongwei <79183004+leecongwei@users.noreply.github.com>
2021-11-25 13:56:33 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,12 +1,12 @@
-# Tool Name :- termux-toolx
-# Author :- 黑k江湖
-# Date :- 15/10/2020
-
 import os
 import sys
 from time import sleep
 from modules.logo import *
 from modules.system import *
+
+yellow="\033[1;33m"
+blue="\033[1;34m"
+nc="\033[00m"
 
 class tool:
   @classmethod
@@ -15,14 +15,14 @@ class tool:
       system=sys()
       os.system("clear")
       logo.ins_tnc()
-      inp=input("\033[1;33m 刚刚改好了一些漏洞，帮个忙看看 [Y/yes]> \033[00m")                               
-      if inp=="Y" or inp=="yes":
+      inp=input(f"{yellow}请问你要下载神器Tool-X吗？ [Y/n]> {nc}")
+      if inp=="y" or inp=="Y":
         os.system("clear")
         logo.installing()
         if system.sudo is not None:
           #require root permission
           if os.path.exists(system.conf_dir+"/Tool-X"):
-            pass                                                                                               
+            pass
           else:
             os.system(system.sudo+" mkdir "+system.conf_dir+"/Tool-X")
           os.system(system.sudo+" cp -r modules core Tool-X.py "+system.conf_dir+"/Tool-X")
@@ -34,12 +34,12 @@ class tool:
           if os.path.exists(system.bin+"/Tool-X") and os.path.exists(system.conf_dir+"/Tool-X"):
             os.system("clear")
             logo.ins_sc()
-            tmp=input("\033[1;36m ##> \033[00m")
+            tmp=input(f"{blue}Tool-X{nc}@{blue}space {yellow}$ {nc}")
             break
           else:
             os.system("clear")
             logo.not_ins()
-            tmp=input("\033[1;36m ##> \033[00m")
+            tmp=input(f"{blue}Tool-X{nc}@{blue}space {yellow}$ {nc}")
             break
         else:
           if os.path.exists(system.conf_dir+"/Tool-X"):
@@ -47,7 +47,7 @@ class tool:
           else:
             os.system("mkdir "+system.conf_dir+"/Tool-X")
           os.system("cp -r modules core Tool-X.py "+system.conf_dir+"/Tool-X")
-          os.system("cp -r core/Tool-X"+system.bin)
+          os.system("cp -r core/Tool-X "+system.bin)
           os.system("cp -r core/toolx "+system.bin)
           os.system("chmod +x "+system.bin+"/Tool-X")
           os.system("chmod +x "+system.bin+"/toolx")
@@ -55,15 +55,13 @@ class tool:
           if os.path.exists(system.bin+"/Tool-X") and os.path.exists(system.conf_dir+"/Tool-X"):
             os.system("clear")
             logo.ins_sc()
-            tmp=input("\033[1;36m ##> \033[00m")
+            tmp=input(f"{blue}Tool-X{nc}@{blue}space {yellow}$ {nc}")
             break
           else:
             os.system("clear")
             logo.not_ins()
-            tmp=input("\033[1;36m ##> \033[00m")
+            tmp=input(f"{blue}Tool-X{nc}@{blue}space {yellow}$ {nc}")
             break
-      else:
-        break
       else:
         break
 
